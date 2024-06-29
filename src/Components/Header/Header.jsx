919f1ed5dac1528ddbec5bbcafc4190923ec1d51
Jashany <jsingh12_be22@thapar.edu>
2024-06-29 09:21:33 +0530

    push

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,24 @@
 import { useSelector } from "react-redux";
 import styles from "./Header.module.css";
+import { clearUser } from "../../Slices/AuthSlice";
+import { useDispatch } from "react-redux";
 const Header = () => {
     const user = useSelector((state) => state.auth.auth);
-
+    const dispatch = useDispatch();
+    const logout = () => {
+        dispatch(clearUser());
+    };
     return ( 
         <div className={styles.header}>
+            <div style={{display:"flex",justifyContent:"space-between",width:"100%"}}>
+
             <h1>Salon Dekho</h1>
             <h2>{user?.phoneNumber}</h2>
+            
+            </div>
+            <button onClick={logout}>
+                Logout
+            </button>
         </div>
      );
 }
--- src/Pages/ShopDetials/ShopDetails.jsx
+++ src/Pages/ShopDetials/ShopDetails.jsx
@@ -1,16 +1,28 @@
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import styles from "./ShopDetails.module.css";
 import Header from "../../Components/Header/Header";
 import SideBar from "../../Components/Sidebar/Sidebar";
+
 const ShopDetails = () => {
     const state = useLocation();
     const allImages = [...state?.state?.StorePhotos, state?.state?.CoverImage];
+
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleImageClick = (image) => {
+        setSelectedImage(image);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedImage(null);
+    };
+
     return ( 
         <div className={styles.main}>
             <Header />
             <SideBar />
             <div className={styles.shop}>
-               
                 <div className={styles.artists}>
                     <h2>Artists</h2>
                     <div className={styles.box}>
@@ -26,11 +38,8 @@ const ShopDetails = () => {
                     </div>
                 </div>
                 <div className={styles.services}>
-                    <h2>
-                        Services
-                    </h2>
+                    <h2>Services</h2>
                     <div className={styles.box}>
-
                     {state?.state?.Services.map((service) => {
                         return (
                             <div key={service._id} className={styles.service}>
@@ -44,9 +53,7 @@ const ShopDetails = () => {
                     </div>
                 </div>
                 <div className={styles.offers}>
-                    <h2>
-                        Offers
-                    </h2>
+                    <h2>Offers</h2>
                     <div className={styles.box}>
                     {state?.state?.offers.map((offer) => {
                         return (
@@ -59,31 +66,37 @@ const ShopDetails = () => {
                     </div>
                 </div>
                 <div className={styles.photos}>
-                    <h2>
-                        Photos
-                    </h2>
+                    <h2>Photos</h2>
                     <div className={styles.images}>
-                    {allImages.map((photo) => {
-                        
+                    {allImages.map((photo, index) => {
                         return (
-                            <div key={photo._id} className={styles.photo}>
-                                <img src={photo} alt="salon" />
+                            <div key={index} className={styles.photo}>
+                                <img 
+                                    src={photo} 
+                                    alt="salon" 
+                                    onClick={() => handleImageClick(photo)} 
+                                />
                             </div>
                         )
                     })}
                     </div>
                 </div>
                 <div className={styles.brocher}>
-                    <h2>
-                        Brocher
-                    </h2>
+                    <h2>Brochure</h2>
                     <div className={styles.brocherImage}>
-                        <img src={state?.state?.Brochure} alt="brocher" />
+                        <img src={state?.state?.Brochure} alt="brochure" />
                     </div>
                 </div>
             </div>
+
+            {selectedImage && (
+                <div className={styles.modal} onClick={handleCloseModal}>
+                    <span className={styles.close}>&times;</span>
+                    <img className={styles.modalContent} src={selectedImage} alt="Full Size" />
+                </div>
+            )}
         </div>
-     );
+    );
 }
  
 export default ShopDetails;
